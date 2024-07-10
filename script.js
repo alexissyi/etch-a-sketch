cbb1f8b88d9db8d76051a358368f8e4ef6c41908
Alexis <172384082+alexissyi@users.noreply.github.com>
2024-07-10 16:07:10 +0545

    Implement etch-a-sketch board with adjustable sizing

---
+++ script.js
@@ -0,0 +1,51 @@
+let container = document.querySelector(".container");
+const body = document.querySelector("body");
+const button = document.querySelector("button");
+const containerSize = 480;
+let size = 16;
+
+function createGrid(size) {
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < size; j++) {
+      const squareNumber = i * size + j;
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.setAttribute("id", "square" + squareNumber);
+      const squareSize = containerSize / size + "px";
+      square.style.height = squareSize;
+      square.style.width = squareSize;
+      row.appendChild(square);
+    }
+    container.appendChild(row);
+  }
+  container.addEventListener("mouseover", changeColor);
+}
+
+function changeColor(event) {
+  console.log("in here");
+  const target = event.target;
+  const id = target.id;
+  console.log(id);
+  if (id) {
+    const square = document.querySelector("#" + id);
+    square.style.backgroundColor = "pink";
+  }
+}
+
+function changeSize() {
+  const newSize = parseInt(prompt("Enter a new size (max 100):"));
+  if (newSize <= 100 && newSize % 1 === 0 && newSize > 0) {
+    const parent = container.parentNode;
+    parent.removeChild(container);
+    container = document.createElement("div");
+    container.classList.add("container");
+    parent.appendChild(container);
+    createGrid(newSize);
+  }
+}
+
+createGrid(size);
+
+button.addEventListener("click", changeSize);
